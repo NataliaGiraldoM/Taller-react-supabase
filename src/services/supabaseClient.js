972e7e3d0ae514a91f8618d9f87f2b7ae06de25c
NataliaGiraldoM <natalia.giraldom.col@upb.edu.co>
2026-03-17 21:53:35 -0500

    Agregar variable de llave vacía

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://xamfrnrurcehnbapjonq.supabase.co"
+const supabaseKey = ""
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
